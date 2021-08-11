7e47662ff2eae5a6ec470decb4502b815be42e1c
lilili <lilili@home_win10>
2021-08-11 22:52:22 +0800

    feat: update Policy Gradient

--- Policy_Gradient/model.py
+++ Policy_Gradient/model.py
@@ -94,5 +94,41 @@ class Policy_Gradient_2(Policy_Gradient):
         grads = tape.gradient(loss, self.policy_net.trainable_variables)
         self.opt.apply_gradients(zip(grads, self.policy_net.trainable_variables))
         self.state_list, self.action_list, self.reward_list = [], [], []
-    
         
+        
+        
+class Policy_Gradient_3(Policy_Gradient):
+    def __init__(self, state_dim, action_dim, lr, gamma, hidden_dim):
+        Policy_Gradient.__init__(self, state_dim, action_dim, lr, gamma, hidden_dim)
+        self.opt = optimizers.Adam(self.lr)
+    
+    def _build_net(self):
+        policy_net = models.Sequential([
+            layers.Dense(units=self.hidden_dim, input_dim=self.state_dim, activation="relu"),
+            layers.Dense(units=self.action_dim, input_dim=self.hidden_dim, activation="softmax")
+        ])
+        return policy_net
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        prob = self.policy_net(s).numpy().flatten()
+        action = np.random.choice(self.action_dim, 1, p=prob)[0]
+        return action
+    
+    def learn(self):
+        episode_length = len(self.state_list)
+        discount_rewards = self._discount_and_norm_rewards()
+        episode_state = np.vstack(self.state_list)
+        episode_action = np.array(self.action_list)
+        episode_index = np.arange(episode_length)
+        y = np.zeros((episode_length, self.action_dim))
+        y[episode_index, episode_action] = discount_rewards
+        
+        with tf.GradientTape() as tape:
+            episode_action_prob = self.policy_net(episode_state)
+            cross_entropy = tf.losses.categorical_crossentropy(y_true=episode_action,
+                                                               y_pred=episode_action_prob)                                                    
+            loss = tf.reduce_mean(cross_entropy * discount_rewards)
+        grads = tape.gradient(loss, self.policy_net.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.policy_net.trainable_variables))
+        self.state_list, self.action_list, self.reward_list = [], [], []
--- Policy_Gradient/run.py
+++ Policy_Gradient/run.py
@@ -10,9 +10,11 @@ parser = argparse.ArgumentParser("Policy Gradient")
 parser.add_argument("--render", action="store_true")
 parser.add_argument("--lr", type=float, default=0.001)
 parser.add_argument("--gamma", type=float, default=0.95)
-parser.add_argument("--episodes", type=int, default=3000)
+parser.add_argument("--episodes", type=int, default=2000)
 parser.add_argument("--hidden_dim", type=int, default=5)
 args = parser.parse_args()
+
+AGGREGATE_STATS_EVERY = 50
 
 def get_env(env_id):
     env = gym.make(env_id)
@@ -25,9 +27,9 @@ def get_env(env_id):
 def run():
     env, state_dim, action_dim = get_env("CartPole-v0")
     PG_model = model.Policy_Gradient_2(state_dim, action_dim, args.lr, args.gamma, args.hidden_dim)
-    reward_list = []
-    for i in tqdm.trange(args.episodes, ascii=True, unit='episodes'):
-        start_time = time.time()
+    ep_rewards = []
+    aggr_ep_rewards = {'ep':[],'avg':[],'min':[],'max':[]}
+    for i in tqdm.trange(1, args.episodes+1, ascii=True, unit='episodes'):
         s_cur = env.reset()
         reward_sum = 0
         while True:
@@ -52,17 +54,39 @@ def run():
             if done:
                 break
         PG_model.learn()
-        reward_list.append(reward_sum)
-        end_time = time.time()
-        # print("Episode [%d / %d]\tsum reward: %.2f\ttime: %.2fs" %
-        #       (i, args.episodes, reward_sum, end_time-start_time))
-
+        ep_rewards.append(reward_sum)
+        if i % AGGREGATE_STATS_EVERY == 0 or i == 1:
+            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            aggr_ep_rewards['ep'].append(i)
+            aggr_ep_rewards['avg'].append(average_reward)
+            aggr_ep_rewards['min'].append(min_reward)
+            aggr_ep_rewards['max'].append(max_reward)            
+        
     env.close()
 
-    plt.plot(range(args.episodes), reward_list)
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label = 'avg')
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label = 'min')
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label = 'max')
+    plt.legend(loc='upper left')
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
     plt.show()
 
+    state = env.reset()
+    for i in range(5):
+        state = env.reset()
+        done = False
+        while not done:
+            action = PG_model.choose_action(state)
+            next_state, _, done, _ = env.step(action)
+            state = next_state
+            env.render()
 
+    env.close()
+    
+    
 
 
 if __name__ == "__main__":
